7f01db59441f2f55b9acb2a941bbd1681f70bbb2
dsilva2 <dsilva24@stanford.edu>
2025-04-02 14:33:43 -0700

    simple teaching layout update

--- src/components/Teaching.jsx
+++ src/components/Teaching.jsx
@@ -1,19 +1,29 @@
-
-
-
 export const Teaching = () => {
-
-
-    return (
+  return (
     <div className="teaching">
-        <h1>Teaching</h1>
-        <p>I have been trusted to teach a few classes for some reason.</p>
-        <ul>
-            <li><a href="https://hci.cs.stanford.edu/courses/cs147l/2024/au/">CS147L: Cross-Platform Mobile Development: </a>Winter 2023, Fall 2023, Fall 2024</li>
-            <li><a href="https://web.stanford.edu/class/cs194/">CS194: Software Project: </a>Winter 2025, Spring 2025</li>
-        </ul>
-
+      <h1>Teaching</h1>
+      <p>I have been trusted to teach a few classes for some reason.</p>
+      <ul>
+        <li>
+          <a href="https://web.stanford.edu/class/cs194/">
+            CS194: Software Project
+          </a>
+          <ul>
+            <li>Spring 2025</li>
+            <li>Winter 2025</li>
+          </ul>
+        </li>
+        <li>
+          <a href="https://hci.cs.stanford.edu/courses/cs147l/2024/au/">
+            CS147L: Cross-Platform Mobile Development
+          </a>
+          <ul>
+            <li>Fall 2024</li>
+            <li>Fall 2023</li>
+            <li>Winter 2023</li>
+          </ul>
+        </li>
+      </ul>
     </div>
-        )
-
-}
+  );
+};
