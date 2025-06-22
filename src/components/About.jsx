94efe3a59a2ffd79a6e7604b1fa71c990d7b7662
dsilva2 <dsilva24@stanford.edu>
2025-06-21 20:06:01 -0400

    update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export const About = (props) => {
     return (
         <div {...props}>
             <p>
-I am an aspiring NFL superstar. Given my 5'10" frame and painfully average athletic ability, I have also decided to pursue some backup options. As a result, I just recently wrapped up my M.S. in Computer Science at Stanford University.
+I am an aspiring NFL superstar. Given my 5'10" frame and painfully average athletic ability, I have also decided to pursue some backup options. As a result, I just recently wrapped up my M.S. in Computer Science at Stanford University, concentrating in AI and HCI (Human-Computer Interaction).
 </p>
 {/* <p>
 My research interests lie in using generative AI to scale high quality and accessible education. 
